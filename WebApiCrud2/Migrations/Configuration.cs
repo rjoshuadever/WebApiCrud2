0d04eacd8208388766cf39f9ebf56e413fc046e7
Josh D <rjoshuadever@gmail.com>
2023-01-13 09:21:14 -0800

    init remove commented code

--- WebApiCrud2/Migrations/Configuration.cs
+++ WebApiCrud2/Migrations/Configuration.cs
@@ -24,18 +24,6 @@ namespace WebApiCrud2.Migrations
 
                 );
 
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
